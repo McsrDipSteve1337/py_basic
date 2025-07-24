8f973c0a0e3079ed1c2e4910c3241ccc6d058ef0
McsrDipSteve1337 <hienduyhoangvp2010@gmail.com>
2025-07-24 23:03:16 +0700

    Solve day 1

---
+++ Basic _ In-Out/Bai3.py
@@ -0,0 +1,4 @@
+name=input()
+age=int(input())
+grade=input()
+print(f"Ho ten: {name}, Tuoi: {age}, Lop: {grade}")
---
+++ Basic _ In-Out/Bai4.py
@@ -0,0 +1,3 @@
+a,b,c=map(int,input().split())
+ans=(a+b+c)/3
+print(f"{ans:.2f}")
